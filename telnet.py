2e5f42fbb28f463501ed8bdf08c7ce795cb33f3f
Kame <kame@duck.com>
2024-03-26 06:56:33 +0900

    Formatted code using Black

--- telnet.py
+++ telnet.py
@@ -6,8 +6,9 @@ from twisted.internet import reactor, protocol, endpoints
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 class SimpleTelnetProtocol(basic.LineReceiver):
-    delimiter = b'\n'
+    delimiter = b"\n"
     maxAttempts = 3
 
     def __init__(self):
@@ -42,32 +43,39 @@ class SimpleTelnetProtocol(basic.LineReceiver):
                 self.transport.loseConnection()
         else:
             log.msg(f"Received username attempt: {line}")
-            self.promptForPassword() 
+            self.promptForPassword()
 
     def connectionLost(self, reason):
         log.msg("Connection lost")
 
+
 class SimpleTelnetFactory(protocol.ServerFactory):
     protocol = SimpleTelnetProtocol
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Run a simple Telnet honeypot server.')
-    parser.add_argument('--host', type=str, default='0.0.0.0', help='Host to bind the Telnet server to.')
-    parser.add_argument('--port', type=int, default=2323, help='Port to bind the Telnet server to.')
+    parser = argparse.ArgumentParser(description="Run a simple Telnet honeypot server.")
+    parser.add_argument(
+        "--host", type=str, default="0.0.0.0", help="Host to bind the Telnet server to."
+    )
+    parser.add_argument(
+        "--port", type=int, default=2323, help="Port to bind the Telnet server to."
+    )
     args = parser.parse_args()
 
     LOG_FILE_PATH = os.path.join(script_dir, "telnet_honeypot.log")
     print(f"TELNET HONEYPOT ACTIVE ON HOST: {args.host}, PORT: {args.port}")
     print(f"ALL attempts will be logged in: {LOG_FILE_PATH}")
-    
-    log_observer = log.FileLogObserver(open(LOG_FILE_PATH, 'a'))
+
+    log_observer = log.FileLogObserver(open(LOG_FILE_PATH, "a"))
     log.startLoggingWithObserver(log_observer.emit, setStdout=False)
 
     telnet_factory = SimpleTelnetFactory()
-    
+
     endpoint = endpoints.TCP4ServerEndpoint(reactor, args.port, interface=args.host)
     endpoint.listen(telnet_factory)
     reactor.run()
+
 
 if __name__ == "__main__":
     main()
